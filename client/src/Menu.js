d0a70ec729c5a1fd8c415df3a38cf8f7d2ea8725
Sam Grace <sam@festicket.com>
2018-07-22 00:25:34 +0100

    :art: starting to work

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -10,35 +10,17 @@ import "./Menu.css";
 
 export const Menu = () => (
   <Router>
-    <div className={"menu-router"}>
-      <div style={{ backgroundColor: "blue" }}>
-        <NavLink
-          to="/burgers"
-          className="title"
-          activeClassName="active-menu-page"
-        >
+    <div className="outer-container">
+      <div className="inner-container">
+        <NavLink className="title burgers" to="/burgers">
           Hamburgers
         </NavLink>
         <Route path={"/burgers"} component={Burgers} />
-      </div>
-
-      <div style={{ backgroundColor: "orange" }}>
-        <NavLink
-          to="/pizza"
-          className="title"
-          activeClassName="active-menu-page"
-        >
+        <NavLink className="title pizza" to="/pizza">
           Pizza
         </NavLink>
         <Route path={"/pizza"} component={Pizza} />
-      </div>
-
-      <div style={{ backgroundColor: "pink" }}>
-        <NavLink
-          to="/dessert"
-          className="title"
-          activeClassName="active-menu-page"
-        >
+        <NavLink className="title dessert" to="/dessert">
           Dessert
         </NavLink>
         <Route path={"/dessert"} component={Dessert} />
@@ -48,22 +30,60 @@ export const Menu = () => (
 );
 
 const Burgers = match => (
-  <ul className="menu-body">
+  <ul className="scrollable-content burgers">
     <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li>
+    <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
+    <li>Chicken Burger</li>
+    <li>asd</li> <li>Cheese Burger</li>
     <li>Chicken Burger</li>
     <li>asd</li>
   </ul>
 );
 
 const Pizza = () => (
-  <ul className="menu-body">
+  <ul className="scrollable-content pizza">
     <li>Hawiaan</li>
     <li>Vege Supreme</li>
+    <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
+    <li>Vege Supreme</li> <li>Hawiaan</li>
   </ul>
 );
 
 const Dessert = () => (
-  <ul className="menu-body">
+  <ul className="scrollable-content dessert">
     <li>Pavlova</li>
     <li>Ice Cream Sundae</li>
   </ul>
