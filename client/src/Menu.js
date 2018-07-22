e95629acf16bcaf61eacedf555b7fc64f3fc75d5
Sam Grace <sam@festicket.com>
2018-07-22 14:46:08 +0100

    fix layout bug

--- client/src/Menu.js
+++ client/src/Menu.js
@@ -8,79 +8,56 @@ import {
 
 import "./Menu.css";
 
-/**
- * Making this a component will make it rerender when we change url so it can use
- * the correct value for "to".
- * PureComponent this wouldn't fwork
- */
-class DynamicLink extends React.Component {
-  render() {
-    console.log("rerender");
-    const { to, className, children } = this.props;
-    const pathname = window.location.pathname;
-    return (
-      <Link className={`title ${className}`} to={pathname === to ? "" : to}>
-        {children}
-      </Link>
-    );
-  }
-}
-
 export const Menu = () => {
   console.log("render");
   return (
     <Router>
       <div className="outer-container">
         <div className="inner-container">
-          {/*<DynamicLink className="burgers" to="/burgers">*/}
-          {/*Burgers*/}
-          {/*</DynamicLink>*/}
-          {/*<Route path={"/burgers"} component={Burgers} />*/}
           <MenuSection
-            color={"blue"}
             sectionName={"Burgers"}
-            path={"/burgers"}
-            menuItems={[
+            sectionPath={"/burgers"}
+            sectionColor={"blue"}
+            sectionItems={[
               "Philly Cheese Burger",
               "Mr Big Chicken",
               "Reuben Grilled Cheese"
             ]}
           />
-          {/*<DynamicLink className="pizza" to="/pizza">*/}
-          {/*Pizza*/}
-          {/*</DynamicLink>*/}
-          {/*<Route path={"/pizza"} component={Pizza} />*/}
-          {/*<DynamicLink className="dessert" to="/dessert">*/}
-          {/*Dessert*/}
-          {/*</DynamicLink>*/}
-          <Route path={"/dessert"} component={Dessert} />
         </div>
       </div>
     </Router>
   );
 };
 
-const MenuSection = ({ color, sectionName, path, menuItems }) => {
-  const primaryStyles = { backgroundColor: color };
+const MenuSection = ({
+  sectionColor,
+  sectionName,
+  sectionPath,
+  sectionItems
+}) => {
+  const primaryStyles = { backgroundColor: sectionColor };
   return (
     <React.Fragment>
       <NavLink
         className="title"
-        activeClassName="hide-active-menu"
+        activeClassName="hide-active-navlink"
         style={primaryStyles}
-        to={path}
+        to={sectionPath}
       >
         {sectionName}
       </NavLink>
+
       <Route
-        path={path}
+        path={sectionPath}
         render={() => (
           <React.Fragment>
             <Link className="title" style={primaryStyles} to="/">
               {sectionName}
             </Link>
+
             <ul className="scrollable-content" style={primaryStyles}>
-              {menuItems.map(item => <li key={item}>{item}</li>)}
+              {sectionItems.map(item => <li key={item}>{item}</li>)}
             </ul>
           </React.Fragment>
         )}
@@ -88,82 +65,3 @@ const MenuSection = ({ color, sectionName, path, menuItems }) => {
     </React.Fragment>
   );
 };
-
-const Burgers = () => (
-  <React.Fragment>
-    <NavLink
-      className="burgers title"
-      to="/burgers"
-      activeClassName="active-menu"
-    >
-      Burgers
-    </NavLink>
-    <Route
-      path={"/burgers"}
-      render={() => (
-        <React.Fragment>
-          <Link className="burgers title" to="/">
-            Burgers
-          </Link>
-          <ul className="scrollable-content burgers">
-            <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li>
-            <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li> <li>Cheese Burger</li>
-            <li>Chicken Burger</li>
-            <li>asd</li>
-          </ul>
-        </React.Fragment>
-      )}
-    />
-  </React.Fragment>
-);
-
-const Pizza = () => (
-  <ul className="scrollable-content pizza">
-    <li>Hawiaan</li>
-    <li>Vege Supreme</li>
-    <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-    <li>Vege Supreme</li> <li>Hawiaan</li>
-  </ul>
-);
-
-const Dessert = () => (
-  <ul className="scrollable-content dessert">
-    <li>Pavlova</li>
-    <li>Ice Cream Sundae</li>
-  </ul>
-);
